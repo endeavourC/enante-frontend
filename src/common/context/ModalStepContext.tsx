88a768cc1bac0999cd5af81dcec01e3b4e88659d
Marcin Czaiński <35568078+endeavourC@users.noreply.github.com>
2023-05-01 17:00:06 +0200

    Clear warnings

--- src/common/context/ModalStepContext.tsx
+++ src/common/context/ModalStepContext.tsx
@@ -1,10 +1,4 @@
-import {
-	createContext,
-	useState,
-	useCallback,
-	useMemo,
-	useEffect,
-} from 'react';
+import { createContext, useState, useCallback, useMemo } from 'react';
 import { Step } from '@/common/types/Step';
 
 type ModalContextType = {
